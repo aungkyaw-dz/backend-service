52db84429a6f8873faaf9b60efe9a249ac1f56b5
aung_kyaw <aung_k@kkventureglobal.com>
2022-06-20 21:19:15 +0700

    init

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,6 @@ import express, { Express, Request, Response } from 'express';
 import  CONFIG  from './config/env'
 import dbConnection from './config/db_connection'
 import routers from './routers'
-const cors = require('cors');
 import fileUpload from 'express-fileupload';
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -19,7 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload())
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
 app.use('/', routers);
 dbConnection
 
