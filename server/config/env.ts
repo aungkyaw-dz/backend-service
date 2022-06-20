b60cd2ce1b2e2e54e29e7af616ed35d882d4364c
aung_kyaw <aung_k@kkventureglobal.com>
2022-06-20 20:58:23 +0700

    update

--- server/config/env.ts
+++ server/config/env.ts
@@ -2,13 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    RUNNING_PORT: parseInt(process.env.PORT || '3001'),
+    RUNNING_PORT: parseInt(process.env.PORT || 'us-cdbr-east-05.cleardb.net'),
     NODE_ENV: process.env.NODE_ENV,
     MYSQLDB: {
       HOST: process.env.DB_HOST || 'localhost',
-      DATABASE_NAME: process.env.DB_NAME || 'the_urban_tech',
-      USERNAME: process.env.DB_USERNAME || 'root',
-      PASSWORD: process.env.DB_PASSWORD || 'admin123',
+      DATABASE_NAME: process.env.DB_NAME || 'heroku_12826af6ab28a33',
+      USERNAME: process.env.DB_USERNAME || 'b5117301bb5162',
+      PASSWORD: process.env.DB_PASSWORD || 'ee5cbe3f',
     },
     IMAGEKIT: {
       PUBLIC: process.env.IMAGEKIT_PUBLIC ||"public_Gy4558uOUfOf6C5emV6g81ahTOs=",
