2d7458d60862d6b16471c34661df8ba9de2ce9f5
aung_kyaw <aung_k@kkventureglobal.com>
2022-07-03 19:30:46 +0700

    update

--- server/config/env.ts
+++ server/config/env.ts
@@ -22,8 +22,9 @@ export default {
       EXPIRES_IN: process.env.JWT_EXPIRES_IN
     },
     PINATA: {
-      KEY: process.env.PINATA_KEY || "",
-      SECRET: process.env.PINATA_SECRET|| "",
+      KEY: process.env.PINATA_KEY || "9d70e2cf4d7c5c4d1186",
+      SECRET: process.env.PINATA_SECRET|| "5906c9b14610e80c0b3adb8f3d65144ed77267797b8594bafcbb8146a5ca23ef",
+      JWT: process.env.PINATA_JWT|| "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIwOGFhMGQ0Zi05NzVkLTQyMzUtOGE5Ny0zMDFjYWRmYmVhNmQiLCJlbWFpbCI6ImF1bmdfa0Bra3ZlbnR1cmVnbG9iYWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZX0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjlkNzBlMmNmNGQ3YzVjNGQxMTg2Iiwic2NvcGVkS2V5U2VjcmV0IjoiNTkwNmM5YjE0NjEwZTgwYzBiM2FkYjhmM2Q2NTE0NGVkNzcyNjc3OTdiODU5NGJhZmNiYjgxNDZhNWNhMjNlZiIsImlhdCI6MTY1MDQ0Nzc1N30.RnZm-3CK1tuXf8djNew7mZ9vXUINM-IW7qRYKS3KSGo",
     }
   };
   
